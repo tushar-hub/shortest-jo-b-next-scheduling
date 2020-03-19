92db41aa1586e7fcd6f2028bb1eb47c4d3016e73
tushar-hub <62297423+tushar-hub@users.noreply.github.com>
2020-03-19 22:52:15 +0530

    Update sjf.cpp

--- sjf.cpp
+++ sjf.cpp
@@ -1,2 +1,10 @@
 #include<iostream.h>
 #include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("\t\t\t--(OPERATING SYSTEM CA ON SCHEDULING)---");
+  long int n,j=0,i=0;
+  printg("PLEASE ENTER HT ENUMBER OF PROCESSES WITH YOU WANT TO PROCEED :");
+  scanf("%ld",&n);
+  avg_turnaround,avg_waiting,burstTime
